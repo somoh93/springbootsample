c42a443368f6578b358203b62089a7911b303b68
Somdas M <somdas.m@gmail.com>
2021-07-26 19:11:55 +0530

    Spring starter project

---
+++ src/test/java/com/somlabs/springbootsample/SpringbootsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somlabs.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
